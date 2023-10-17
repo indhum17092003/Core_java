508e73aca5fbf2623e392f09395db03949605ec7
Indhuja <Indhuja@INDHU>
2023-10-17 19:15:03 +0530

    first  commit

---
+++ c29/src/com/cg/dayone/Operators.java
@@ -0,0 +1,10 @@
+package com.cg.dayone;
+
+public class Operators {
+
+	public static void main(String[] args) {
+		// TODO uto-generated method stub
+         System.out.println("Hello World");
+	}
+
+}
